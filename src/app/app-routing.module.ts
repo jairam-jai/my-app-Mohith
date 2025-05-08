c4e82f500a0f45534abb5c3c828f51ec8daf25b0
jairam-jai <jayaram8688200@gmail.com>
2025-05-08 11:33:15 +0530

    chang code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'home',component:HomeComponent},
+  {path:'dashbord',component:DashbordComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
